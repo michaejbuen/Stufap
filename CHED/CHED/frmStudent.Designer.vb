e3772f26cc3db1b36fc5697b3ea517cb8ec2bc04
michaeljbuen <michaeljosephbuen27@gmail.com>
2018-05-02 21:50:25 +0800

    stufap

--- CHED/CHED/frmStudent.Designer.vb
+++ CHED/CHED/frmStudent.Designer.vb
@@ -269,9 +269,9 @@ Partial Class frmStudent
         Me.Label3.Image = CType(resources.GetObject("Label3.Image"), System.Drawing.Image)
         Me.Label3.Location = New System.Drawing.Point(197, 254)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(143, 29)
+        Me.Label3.Size = New System.Drawing.Size(153, 29)
         Me.Label3.TabIndex = 20
-        Me.Label3.Text = "Add Student"
+        Me.Label3.Text = "Add Students"
         '
         'pi_panel
         '
